954e1a59de2cc0a6861ab4a4c6d109c7aca12d50
lhmanrique <lhmanrique@gmail.com>
2025-08-13 15:55:52 -0600

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,6 +5,6 @@ import tailwind from "@astrojs/tailwind";
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind()],
-  site: "https://nexusstores.github.io",
-  base: "",
+  site: "https://lhmanrique.github.io",
+  base: "nexusstores",
 });
